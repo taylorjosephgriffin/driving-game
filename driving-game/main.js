905ff974a11e4fc331a75a15b2e40d22e1ab9740
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-06-13 13:39:48 -0700

    Added a move method to the Car class that updates it's location according to it's speed and direction. #2

--- driving-game/main.js
+++ driving-game/main.js
@@ -9,10 +9,27 @@ class Car {
     this.direction = direction
     this.$img.className = direction
   }
+  move() {
+    switch (this.direction) {
+      case 'north':
+        this.location[1] += this.speed
+        break
+      case 'south':
+        this.location[1] -= this.speed
+        break
+      case 'east':
+        this.location[0] += this.speed
+        break
+      case 'west':
+        this.location[0] -= this.speed
+        break
+      }
+  }
 }
 
 const $mustang = document.createElement('img')
 $mustang.setAttribute('src', 'images/car1.png')
+$mustang.setAttribute('style', 'position: relative')
 
 let mustang = new Car($mustang, 2, 'east', [0, 0])
 
