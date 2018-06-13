a4a8000cf9f395da96782b076e97a4de09f0e1b5
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-06-13 13:14:41 -0700

    Added a 'keydown' event listener for arrow keys on the document that faces the car in the direction pressed. #3

--- driving-game/main.js
+++ driving-game/main.js
@@ -19,3 +19,21 @@ $mustang.classList.add(mustang.direction)
 
 const $gameCont = document.querySelector('.game-container')
 $gameCont.appendChild($mustang)
+
+document.body.addEventListener('keydown', function (event) {
+  const key = event.key
+  switch (key) {
+    case 'ArrowUp':
+      mustang.turn('north')
+      break
+    case 'ArrowDown':
+      mustang.turn('south')
+      break
+    case 'ArrowLeft':
+      mustang.turn('west')
+      break
+    case 'ArrowRight':
+      mustang.turn('east')
+      break
+  }
+})
