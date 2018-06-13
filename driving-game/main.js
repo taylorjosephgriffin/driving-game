bd2714fdfc22b809c8b1e24c242e52ff6f97e639
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-06-13 15:58:59 -0700

    Refactored main.js to make isRunning a property of the Car constructor instead of making it a global variable. #8

--- driving-game/main.js
+++ driving-game/main.js
@@ -1,9 +1,10 @@
 class Car {
-  constructor($img, speed, direction, location) {
+  constructor($img, speed, direction, location, isRunning) {
     this.$img = $img
     this.speed = speed
     this.direction = direction
     this.location = location
+    this.isRunning = isRunning
   }
   turn(direction) {
     this.direction = direction
@@ -47,7 +48,7 @@ class Car {
 const $mustang = document.createElement('img')
 $mustang.setAttribute('src', 'images/car1.png')
 
-let mustang = new Car($mustang, 5, 'east', [0, 0])
+let mustang = new Car($mustang, 5, 'east', [0, 0], false)
 mustang.$img.setAttribute('style', 'position: relative')
 
 $mustang.classList.add(mustang.direction)
@@ -73,16 +74,14 @@ document.body.addEventListener('keydown', function (event) {
   }
 })
 
-let isRunning = false
-
 document.body.addEventListener('keydown', function (event) {
   const key = event.keyCode
-  if (key === 32 && !isRunning) {
-    isRunning = true
+  if (key === 32 && !mustang.isRunning) {
+    mustang.isRunning = true
     mustang.start()
   }
-  else if (key === 32 && isRunning) {
-    isRunning = false
+  else if (key === 32 && mustang.isRunning) {
+    mustang.isRunning = false
     mustang.stop()
   }
 })
